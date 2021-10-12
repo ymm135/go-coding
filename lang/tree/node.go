0746c462aaef2156a9021825e6a264169c2f116c
xiao.ming <1210919685@qq.com>
2021-10-12 20:16:08 +0800

    增加结构体的笔记

--- lang/tree/node.go
+++ lang/tree/node.go
@@ -7,6 +7,9 @@ type Node struct {
 	Left, Right *Node
 }
 
+// 声明方法的方式很不一样, 需要声明为Node接受者的
+// func Print(node Node) { print(node) 这样调用也是一样的, 
+// 这里的node也是值传递, 也可以使用node *Node
 func (node Node) Print() {
 	fmt.Print(node.Value, " ")
 }
@@ -20,6 +23,7 @@ func (node *Node) SetValue(value int) {
 	node.Value = value
 }
 
+// 工厂函数
 func CreateNode(value int) *Node {
-	return &Node{Value: value}
+	return &Node{Value: value} //返回局部变量的地址, 不需要关心分配在堆还是栈上, go有垃圾回收机制
 }
